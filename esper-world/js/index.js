2c408964b63a4ed342c008be9020f63a59736b3e
Corell Brown <corellcb@gmail.com>
2021-04-17 12:34:59 -0400

    scrollbars removed. mobile changes made.

--- esper-world/js/index.js
+++ esper-world/js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $("button").click( () => {
+            $(".flipbook").turn("page", 4);
+    })
+
     function loadApp() {
 
         // Create the flipbook
@@ -6,11 +10,11 @@ $(document).ready(function () {
         $('.flipbook').turn({
             // Width
 
-            width: 922,
+            width: 1152,
 
             // Height
 
-            height: 600,
+            height: 750,
 
             // Elevation
 
@@ -43,5 +47,62 @@ $(document).ready(function () {
         });
     }
 
-    loadApp();
+    function loadMobileApp() {
+
+        // Create the flipbook
+
+        $('.flipbook').turn({
+            // Width
+
+            width: 576,
+
+            // Height
+
+            height: 750,
+
+            // Elevation
+
+            elevation: 40,
+
+            // Enable gradients
+
+            gradients: true,
+
+            // Auto center this flipbook
+
+            autoCenter: true,
+
+            display: "single",
+
+            when: {
+                turning: function (event, page, view) {
+                    $('.embed-container').css('display', 'none');
+                },
+
+                turned: function (event, page, view) {
+                    console.log(page, view);
+                    if (page === 4) {
+                        $('.embed-container').css('width', '400px');
+                        $('.embed-container').css('display', 'inline-block');
+                    } else {
+                        $('.embed-container').css('display', 'none');
+                    }
+                }
+            }
+
+
+        });
+
+        $('.flipbook').turn('removePage', 2);
+        $('.flipbook').turn('removePage', 6);
+        $('.flipbook').turn('removePage', 5);
+    }
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        loadMobileApp();
+        console.log("Mobile");
+    } else {
+        loadApp();
+        console.log("Desktop");
+    }
 });
