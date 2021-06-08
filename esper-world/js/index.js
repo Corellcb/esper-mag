bd857990b69e5bac62f3a202f59f87d0fc9b90c0
Corell Brown <corellcb@gmail.com>
2021-06-08 15:54:22 -0400

    success! bring back the arrow and resizing embeds

--- esper-world/js/index.js
+++ esper-world/js/index.js
@@ -85,6 +85,7 @@ $(document).ready(function () {
                 turned: function (event, page, view) {
                     if (page === 4) {
                         $('.embed-container').css('width', '600px');
+                        $('.embed-container').css('height', '900px');
                         $('.embed-container').css('display', 'inline-block');
                     } else {
                         $('.embed-container').css('display', 'none');
